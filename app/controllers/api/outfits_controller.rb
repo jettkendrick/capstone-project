70dee9d206aec58930e360ae533d2a8162791c01
jettkendrick <jettkendrick@gmail.com>
2018-12-03 10:00:49 -0600

    change show view

--- app/controllers/api/outfits_controller.rb
+++ app/controllers/api/outfits_controller.rb
@@ -1,4 +1,5 @@
 class Api::OutfitsController < ApplicationController
+  before_action :authenticate_user, except: [:index, :show]
   def index
     @outfits = Outfit.all
     @outfits = @outfits.order(:gender_id => :asc)
@@ -9,6 +10,33 @@ class Api::OutfitsController < ApplicationController
     outfit_id = params["id"]
     @outfit = Outfit.find_by(id: outfit_id)
     render "show.json.jbuilder"
+  end
+
+  def create
+    @outfit = Outfit.new(
+      user_id: current_user.id,
+      name: params[:name],
+      description: params[:description],
+      image_url: params[:image_url],
+      gender_id: params[:gender_id],
+      celebrity: params[:celebrity]
+    )
+    if @outfit.save
+      params[:products].each do |product|
+        Product.create(
+          name: product[:name],
+          image: product[:image],
+          product_url: product[:product_url],
+          outfit_id: @outfit.id,
+          gender_id: params[:gender_id],
+          category_id: 1,
+          supplier_id: 1,
+        )
+      end
+      render "show.json.jbuilder"
+    else
+      render json: {errors: @outfit.errors.full_messages}, status: 422
+    end
   end
 
    
--- app/views/api/outfits/show.json.jbuilder
+++ app/views/api/outfits/show.json.jbuilder
@@ -4,8 +4,15 @@ json.celebrity @outfit.celebrity
 json.description @outfit.description 
 json.image @outfit.image_url
 json.gender @outfit.gender.sex
-json.product_names @outfit.product_names
-json.product_images @outfit.product_images
-json.product_urls @outfit.product_urls
+# json.product_names @outfit.product_names
+# json.product_images @outfit.product_images
+# json.product_urls @outfit.product_urls
+json.products @outfit.products.each do |product|
+  json.id product.id
+  json.name product.name
+  json.image product.image
+  json.price product.price
+  json.product_url product.product_url
+end
 json.recommendations @outfit.recommendations
 
